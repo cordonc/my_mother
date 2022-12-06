03b84aab574b960387d42b7ecea5ec47cd3674ed
cordonc <89549006+cordonc@users.noreply.github.com>
2022-12-06 15:44:37 -0500

    asterick css, javascript, width viewport

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+$(window).load(function(){
+
+    $(document).ready(function(){
+     scrollTo(($(document).width() - $(window).width()) / 2, 0);
+});
+});
+
+
+
+
+// window.onload = function(){
+//     document.addEventListener('DOMContentLoaded', function(){console.log('test')})
+//     window.scrollBy(1100,1200)
+//      console.log('i loaded')
+// };
