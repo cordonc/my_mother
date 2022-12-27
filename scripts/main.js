8697f42058d6d917e4fc4f46e0c2827c1c1790c2
cordonc <89549006+cordonc@users.noreply.github.com>
2022-12-27 16:41:18 -0500

    photos

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,27 @@
-$(window).load(function(){
 
-    $(document).ready(function(){
-     scrollTo(($(document).width() - $(window).width()) / 2, 0);
+$(document).ready(function() {
+    $(window).scrollTop($(window).height()/2);
+    $(window).scrollLeft($(window).width()/2);
 });
-});
 
 
 
 
+
+
+
+
+//2nd Attempt
+// $(window).load(function(){
+
+//     $(document).ready(function(){
+//      scrollTo(($(document).width() - $(window).width()) / 2, 0);
+// });
+// });
+
+
+
+//1st Attempt
 // window.onload = function(){
 //     document.addEventListener('DOMContentLoaded', function(){console.log('test')})
 //     window.scrollBy(1100,1200)
